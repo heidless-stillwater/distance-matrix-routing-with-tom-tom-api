791647bce8139dd1c49491de3ff8a5a76b5e68c2
heidless <lockhart.r@gmail.com>
2025-10-01 15:48:07 +0100

    re-deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
+
+
+
+
 echo "switching to Branch main"  
 git checkout main 
 echo "building app..." 
-npm run build 
+export NODE_OPTIONS=--openssl-legacy-provider && npm run build
 echo "deploying files to server..." 
 scp -r build/* heidless@139.162.216.144:/var/www/139.162.216.144/  
 echo "done!"
